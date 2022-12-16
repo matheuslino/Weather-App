94426e4c4ac48c7ff90331a8ed85744808a422fe
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-16 03:01:08 -0500

    [add] custom styles via props

--- src/components/molecules/Searchbar.tsx/Searchbar.tsx
+++ src/components/molecules/Searchbar.tsx/Searchbar.tsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import styles from './style.module.css'
 
-const Searchbar = () => {
+const Searchbar = (props: any) => {
   const render = (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${props?.customClass}`}>
       <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.searchIcon} />
       <input placeholder='toronto' />
     </div>
