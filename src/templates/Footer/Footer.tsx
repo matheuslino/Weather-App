d5d56cf0133867849f2d9766504cc0f228bc6ae4
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-16 02:30:59 -0500

    [add] update component style

--- src/templates/Footer/Footer.tsx
+++ src/templates/Footer/Footer.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import styles from './style.module.css'
 
 const Footer = () => {
   const render = (
-    <footer className={`d-flex flex-column align-items-center`}>
-      <hr className='w-75' />
-      <div className='mt-3'>
-        ©2020{' '}
-        <a href='https://linkedin.com/in/matheus-lino/' target='_blank'>
-          Matheus A. Lino
-        </a>{' '}
-        - All rights reserved.
-      </div>
-    </footer>
+    <section className={styles.footer}>
+      <footer className={`d-flex flex-column align-items-center`}>
+        <hr className='w-75' />
+        <div className='mt-3'>
+          ©2020{' '}
+          <a href='https://linkedin.com/in/matheus-lino/' target='_blank'>
+            Matheus A. Lino
+          </a>{' '}
+          - All rights reserved.
+        </div>
+      </footer>
+    </section>
   )
   return render
 }
