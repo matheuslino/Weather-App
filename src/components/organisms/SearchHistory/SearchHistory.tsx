62cef309a16efd18d8f051ecb201f7e956e0318d
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-16 23:40:27 -0500

    [add] mockup SearchHistory

--- src/components/organisms/SearchHistory/SearchHistory.tsx
+++ src/components/organisms/SearchHistory/SearchHistory.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Container } from 'reactstrap'
 import Box from '../../atoms/Box/Box'
 import Title from '../../atoms/Title/Title'
 import styles from './style.module.css'
 
 const SearchHistory = (props: any) => {
+  const renderList = () => {
+    return (
+      <Container className='pt-2'>
+        <ul>
+          <li>Toronto, ON</li>
+          <li>Mississauga, ON</li>
+          <li>Brampton, ON</li>
+          <li>Vancounver, BC</li>
+          <li>Montreal, QC</li>
+          <li>London, ON</li>
+          <li>-</li>
+        </ul>
+      </Container>
+    )
+  }
+
   const render = (
     <Box className={`${styles.Searchbox} ${props?.className}`}>
       <Title>Search History</Title>
+      {renderList()}
     </Box>
   )
   return render
